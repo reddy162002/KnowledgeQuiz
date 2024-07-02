3091e5b85ba093c8c2eb35ab50770969112b517e
reddy162002 <145621056+reddy162002@users.noreply.github.com>
2024-07-02 23:31:39 +0530

    lottie animation

--- Downloads/Quiz-Application-main/Quiz-Application-main/client/src/pages/user/WriteExam/index.jsx
+++ Downloads/Quiz-Application-main/Quiz-Application-main/client/src/pages/user/WriteExam/index.jsx
@@ -294,6 +294,27 @@ function WriteExam() {
                 </div>
               </div>
             </div>
+            <div className="lottie-animation">
+            {result.verdict === "Pass" && (
+                <lottie-player
+                  src="https://assets2.lottiefiles.com/packages/lf20_ya4ycrti.json"
+                  background="transparent"
+                  speed="1"
+                  loop
+                  autoplay
+                ></lottie-player>
+              )}
+
+              {result.verdict === "Fail" && (
+                <lottie-player
+                  src="https://assets4.lottiefiles.com/packages/lf20_qp1spzqv.json"
+                  background="transparent"
+                  speed="1"
+                  loop
+                  autoplay
+                ></lottie-player>
+              )}
+            </div>
            
             
           </div>
