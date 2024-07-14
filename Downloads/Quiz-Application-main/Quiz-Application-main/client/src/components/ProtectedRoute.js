5b24584bde0769bdd2ac98c01885b384547634c8
reddy162002 <145621056+reddy162002@users.noreply.github.com>
2024-07-14 13:56:03 +0530

    Update ProtectedRoute.js

--- Downloads/Quiz-Application-main/Quiz-Application-main/client/src/components/ProtectedRoute.js
+++ Downloads/Quiz-Application-main/Quiz-Application-main/client/src/components/ProtectedRoute.js
@@ -1,8 +1,6 @@
 import { message } from "antd";
 import React, { useEffect, useState } from "react";
-import { getUserInfo } from "../apicalls/users";
 import { useDispatch, useSelector } from "react-redux";
-import { SetUser } from "../redux/usersSlice.js";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "./firebase.js";
 import { collection, getDocs } from "firebase/firestore";
@@ -10,33 +8,41 @@ import { HideLoading, ShowLoading } from "../redux/loaderSlice";
 
 function ProtectedRoute({ children }) {
   const [menu, setMenu] = useState([]);
+  const [activeRoute, setActiveRoute] = useState(window.location.pathname);
+  const [key, setKey] = useState(new Date().getTime());
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const handleMenuClick = (path) => {
+    setActiveRoute(path);
+    setKey(new Date().getTime()); // Update key to force re-render
+    navigate(path, { replace: true });
+  };
 
   const userMenu = [
     {
       title: "Home",
-      paths: ["/", "/user/write-exam"],
+      paths: ["/"],
       icon: <i className="ri-home-line"></i>,
-      onClick: () => navigate("/"),
+      onClick: () => handleMenuClick("/"),
     },
     {
       title: "Reports",
       paths: ["/user/reports"],
       icon: <i className="ri-bar-chart-line"></i>,
-      onClick: () => navigate("/user/reports"),
+      onClick: () => handleMenuClick("/user/reports"),
     },
     {
       title: "Leaderboard",
       paths: ["/leaderboard"],
       icon: <i className="ri-heart-line"></i>,
-      onClick: () => navigate("/leaderboard"),
+      onClick: () => handleMenuClick("/leaderboard"),
     },
     {
       title: "Profile",
       paths: ["/profile"],
       icon: <i className="ri-user-line"></i>,
-      onClick: () => navigate("/profile"),
+      onClick: () => handleMenuClick("/profile"),
     },
     {
       title: "Logout",
@@ -44,7 +50,7 @@ function ProtectedRoute({ children }) {
       icon: <i className="ri-logout-box-line"></i>,
       onClick: () => {
         localStorage.removeItem("token");
-        navigate("/login");
+        handleMenuClick("/login");
       },
     },
   ];
@@ -52,33 +58,27 @@ function ProtectedRoute({ children }) {
   const adminMenu = [
     {
       title: "Home",
-      paths: ["/", "/user/write-exam"],
+      paths: ["/"],
       icon: <i className="ri-home-line"></i>,
-      onClick: () => navigate("/"),
+      onClick: () => handleMenuClick("/"),
     },
     {
       title: "Quizzes",
       paths: ["/admin/exams", "/admin/exams/add"],
       icon: <i className="ri-file-list-line"></i>,
-      onClick: () => navigate("/admin/exams"),
+      onClick: () => handleMenuClick("/admin/exams"),
     },
     {
       title: "Leaderboard",
       paths: ["/leaderboard"],
       icon: <i className="ri-heart-line"></i>,
-      onClick: () => navigate("/leaderboard"),
+      onClick: () => handleMenuClick("/leaderboard"),
     },
-    // {
-    //   title: "Reports",
-    //   paths: ["/admin/reports"],
-    //   icon: <i className="ri-bar-chart-line"></i>,
-    //   onClick: () => navigate("/admin/reports"),
-    // },
     {
       title: "Profile",
       paths: ["/profile"],
       icon: <i className="ri-user-line"></i>,
-      onClick: () => navigate("/profile"),
+      onClick: () => handleMenuClick("/profile"),
     },
     {
       title: "Logout",
@@ -86,57 +86,57 @@ function ProtectedRoute({ children }) {
       icon: <i className="ri-logout-box-line"></i>,
       onClick: () => {
         localStorage.removeItem("token");
-        navigate("/login");
+        handleMenuClick("/login");
       },
     },
   ];
+
   useEffect(() => {
-  const fetchSubjects = async () => {
+    const fetchSubjects = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Users"));
+        const userData = querySnapshot.docs;
+        const menu = userData.role === "admin" ? adminMenu : userMenu;
+        setMenu(menu);
+      } catch (error) {
+        console.error("Error fetching subjects: ", error);
+      }
+    };
+
+    fetchSubjects();
+  }, []);
+
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const fetchData = async () => {
     try {
-      const querySnapshot = await getDocs(collection(db, "Users"));
-      const userData = querySnapshot.docs;
-      const menu = userData.role === "admin" ? adminMenu : userMenu;
-      setMenu(menu);
+      if (currentUser) {
+        const querySnapshot = await getDocs(collection(db, "Users"));
+        querySnapshot.forEach((doc) => {
+          if (doc.id === currentUser.uid) {
+            const menu = doc.data().role === "admin" ? adminMenu : userMenu;
+            setMenu(menu);
+          }
+        });
+      }
     } catch (error) {
-      console.error("Error fetching subjects: ", error);
+      console.error("Error fetching user data:", error);
     }
   };
 
-  fetchSubjects();
-}, []);
-const [currentUser, setCurrentUser] = useState(null);
-useEffect(() => {
-  const unsubscribe = auth.onAuthStateChanged((user) => {
-    if (user) {
-      setCurrentUser(user);
-    } else {
-      setCurrentUser(null);
-    }
-  });
-  return () => unsubscribe();
-}, []);
-
-const fetchData = async () => {
-  try {
-    if (currentUser) {
-      const querySnapshot = await getDocs(collection(db, "Users"));
-      querySnapshot.forEach((doc) => {
-        if (doc.id === currentUser.uid) {
-          const menu = doc.data().role === "admin" ? adminMenu : userMenu;
-          setMenu(menu);
-        }
-      });
-    }
-  } catch (error) {
-    console.error("Error fetching user data:", error);
-  }
-};
-
-useEffect(() => {
-  fetchData();
-}, [currentUser]);
-
-  const activeRoute = window.location.pathname;
+  useEffect(() => {
+    fetchData();
+  }, [currentUser]);
 
   const getIsActiveOrNot = (paths) => {
     if (paths.includes(activeRoute)) {
@@ -161,32 +161,27 @@ useEffect(() => {
   return (
     <div className="layout">
       <div className="flex w-full h-full h-100">
-        <div className="sidebar" style={{display:"grid", gridTemplateRows:"min-content 10fr"}}>
-        <div style={{fontSize:"3vh",color:"#ffffff", padding:"1vh"}}>
-                  KnowledgeQuiz
-                </div>
+        <div className="sidebar" style={{ display: "grid", gridTemplateRows: "min-content 10fr" }}>
+          <div style={{ fontSize: "3vh", color: "#ffffff", padding: "1vh" }}>
+            KnowledgeQuiz
+          </div>
           <div className="menu">
             {menu.map((item, index) => {
               return (
-                <>
-               
                 <div
-                  className={`menu-item ${
-                    getIsActiveOrNot(item.paths) && "active-menu-item"
-                  }`}
+                  className={`menu-item ${getIsActiveOrNot(item.paths) && "active-menu-item"}`}
                   key={index}
                   onClick={item.onClick}
                 >
                   {item.icon}
                   <span>{item.title}</span>
                 </div>
-                </>
               );
             })}
           </div>
         </div>
         <div className="body">
-          <div className="content">{children}</div>
+          <div className="content" key={key}>{children}</div>
         </div>
       </div>
     </div>
